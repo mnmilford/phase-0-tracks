267df9874643cec7a87717dfd09ddce76f31b742
Michael Milford <mnmilford@gmail.com>
2017-02-18 10:55:20 -0600

    Add questions to collect applicant data

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,1 +1,16 @@
 # Vampire Tester
+
+puts "What is your name?"
+applicant_name = gets.chomp
+
+puts "How many years old are you?"
+years_old = gets.chomp
+
+puts "What year were you born?"
+birth_year = gets.chomp
+
+puts "Our cafeteria serves garlic bread. Should we order some for you?"
+garlic_bread = gets.chomp
+
+puts "Would you like to enroll in the company's health insurance?"
+insurance = gets.chomp
