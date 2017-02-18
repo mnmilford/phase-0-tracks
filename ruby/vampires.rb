ee38cd0fc723b47cf2d8566ef02fb142e02883b8
Michael Milford <mnmilford@gmail.com>
2017-02-18 11:38:56 -0600

    Create 'Age & Garlic Bread' test

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,16 +1,28 @@
 # Vampire Tester
 
+good_applicant = true
+current_year = 2017
+
 puts "What is your name?"
 applicant_name = gets.chomp
 
 puts "How many years old are you?"
-years_old = gets.chomp
+years_old = gets.chomp.to_i
 
 puts "What year were you born?"
-birth_year = gets.chomp
+birth_year = gets.chomp.to_i
 
-puts "Our cafeteria serves garlic bread. Should we order some for you?"
-garlic_bread = gets.chomp
+puts "Our cafeteria serves garlic bread. Should we order some for you? (Yes or No)"
+garlic_bread = gets.chomp.downcase
 
 puts "Would you like to enroll in the company's health insurance?"
 insurance = gets.chomp
+
+# Age & Garlic Bread Test
+if current_year - birth_year == years_old && garlic_bread == "yes"
+  good_applicant = true
+else 
+  good_applicant = false
+end
+
+puts good_applicant
