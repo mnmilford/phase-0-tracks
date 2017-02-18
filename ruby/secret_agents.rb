a06a43cbbbe98ae713eda88439e07b0ed1e66d40
Michael Milford <mnmilford@gmail.com>
2017-02-18 12:55:02 -0600

    Add pseudocode for the encrypt & decrypt methods

---
+++ ruby/secret_agents.rb
@@ -0,0 +1,14 @@
+# Encrypt Method Pseudocode
+
+# Purpose: Advance each letter of a string one letter forward
+# Ask user for a word
+# Analyze each individual letter and move forward one letter in the alphabet
+# Combine those new letters into a new string
+
+
+# Decrypt Method Psuedocode
+
+# Purpose: Reverse the encrypt method
+# Take the encrypted word based on the user's original input
+# Analyze the individual letters and move backward one letter
+# Combine the new letters into a new string
