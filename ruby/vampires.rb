7ffbe11452507e15dd58072371ecd8d5f079498d
Michael Milford <mnmilford@gmail.com>
2017-02-20 09:33:28 -0600

    Create 'correct_year' if statement to simplify test statements

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -3,34 +3,44 @@
 applicant_status = nil
 current_year = 2017
 
-puts "What is your name?"
-applicant_name = gets.chomp
+puts "How many employees are we processing today?"
+number_of_applicants = gets.chomp.to_i
 
-puts "How many years old are you?"
-years_old = gets.chomp.to_i
+while number_of_applicants > 0
 
-puts "What year were you born?"
-birth_year = gets.chomp.to_i
+  puts "What is your name?"
+  applicant_name = gets.chomp
 
-puts "Our cafeteria serves garlic bread. Should we order some for you? (Yes or No)"
-garlic_bread = gets.chomp.downcase
+  puts "How many years old are you?"
+  years_old = gets.chomp.to_i
 
-puts "Would you like to enroll in the company's health insurance? (Yes or No)"
-insurance = gets.chomp.downcase
+  puts "What year were you born?"
+  birth_year = gets.chomp.to_i
 
-# Test Level 1
-if current_year - birth_year == years_old && (garlic_bread == "yes" || insurance == "yes")
-  applicant_status = "Probably not a vampire."
-elsif current_year - birth_year != years_old && (garlic_bread == "no" || insurance == "no")
-  applicant_status = "Probably a vampire."
-elsif current_year - birth_year != years_old && garlic_bread == "no" && insurance == "no"
-  applicant_status = "Almost certainly a vampire."
-elsif applicant_name == "Drake Cula" || applicant_name == "Tu Fang"
-  applicant_status = "Definitely a vampire."
-else
-  applicant_status = "Results inconclusive"
+  puts "Our cafeteria serves garlic bread. Should we order some for you? (Yes or No)"
+  garlic_bread = gets.chomp.downcase
+
+  puts "Would you like to enroll in the company's health insurance? (Yes or No)"
+  insurance = gets.chomp.downcase
+
+  if current_year - birth_year == years_old
+    correct_year = true
+  else
+    correct_year = false
+  end
+
+  if correct_year && (garlic_bread == "yes" || insurance == "yes")
+    applicant_status = "Probably not a vampire."
+  elsif !correct_year && garlic_bread == "no" || !correct_year && insurance == "no"
+    applicant_status = "Probably a vampire."
+  elsif !correct_year && garlic_bread == "no" && insurance == "no"
+    applicant_status = "Almost certainly a vampire."
+  elsif applicant_name == "Drake Cula" || applicant_name == "Tu Fang"
+    applicant_status = "Definitely a vampire."
+  else
+    applicant_status = "Results inconclusive"
+  end
+  number_of_applicants -= 1
+  puts "The result of #{applicant_name}'s application is: #{applicant_status}."
 end
 
-
-
-puts applicant_status
