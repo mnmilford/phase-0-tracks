96e50997e188c3bf4b224194b64d8f36e2d79df4
Michael Milford <mnmilford@gmail.com>
2017-02-21 20:11:25 -0600

    Add some basic pseudocode

---
+++ ruby/hashes.rb
@@ -0,0 +1,9 @@
+# Interior Designer Client Info
+
+# Create an empty hash {} to store client details
+# Ask the designer a few questions to collect client info
+# Questions will presented one by one to collect client's name, age, decor theme, number of rooms,
+# and number of bathrooms
+# Store each response as a new key/value pair and add to the original hash
+# Once all questions have been answered, print out the client info and ask to confirm
+
