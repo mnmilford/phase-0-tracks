5289873f7215c73279be8fcbf1e74e7f8985c4c9
Michael Milford <mnmilford@gmail.com>
2017-02-25 14:57:00 -0600

    Testing methods to increment letter by defined array

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,5 +1,6 @@
 # 5.3 Challenge
 
+
 # Method - Input Spy's regular name - Output first name & last name reversed
 
 def reverse_spy_name(name)
@@ -11,11 +12,22 @@ end
 # Method - Input reversed spy name - Output name with letters shifted to next letter in sequence
 
 def next_letter(name)
+  vowels = ['a','e','i','o','u']
+  consanants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", 
+        "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"]
   name = name.chars
-  name.map! { |x| x.next }
+  name.map! do |x|
+    if vowels.include?(x)
+      vowels.rotate(1)[vowels.index(x)]
+    else 
+      consanants.rotate(1)[consanants.index(x)]
+    end
+  end
   name = name.join('')
   name
 end
+
+
 
 puts "What is the spy's real name?"
 spy_name = gets.chomp
