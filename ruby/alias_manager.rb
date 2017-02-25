8e1a72fdd979834f6d3ef456215bd00a1f3ea77d
Michael Milford <mnmilford@gmail.com>
2017-02-25 14:19:14 -0600

    Create method to reverse spy first/last name

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -3,5 +3,12 @@
 puts "What is the spy's real name?"
 spy_real_name = gets.chomp
 
-spy_real_name = spy_real_name.split(' ').reverse
-p spy_real_name
+# Method - Input Spy's regular name - Output first name & last name reversed
+
+def reverse_spy_name(name)
+  name = name.split(' ').reverse
+  name = name.join(' ')
+  name
+end
+
+p reverse_spy_name(spy_real_name)
