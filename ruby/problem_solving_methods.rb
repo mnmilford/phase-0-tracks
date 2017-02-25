966e75b223a8fce6334feead1c9f6be515716b17
Michael Milford <mnmilford@gmail.com>
2017-02-25 16:03:56 -0600

    Complete working method to find number in array

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -1,2 +1,19 @@
 # 5.6 Challenge - Solving Problems with Data Structures
 
+# Release 0
+
+def search_array(array, number)
+  index = 0
+  while index < array.length
+    array.each do |x|
+      if x == number
+        p index
+      end
+      index += 1
+    end
+  end
+end
+
+arr = [42, 89, 23, 1]
+
+search_array(arr, 23)
