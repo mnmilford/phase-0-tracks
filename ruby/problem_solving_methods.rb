c169e53c3efbcab6264311eb09803ef8a634f780
Michael Milford <mnmilford@gmail.com>
2017-02-26 00:08:23 -0600

    Add bubble sort method that takes any array as argument

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -43,3 +43,20 @@ end
 # If the number to the right is smaller, make the numbers switch places.
 # Keep checking the next number in the list and swap places where necessary
 # Once no more swaps take place, return the new ordered list
+
+def bubble_sort(array)
+  n = array.length
+  loop do
+    swapped = false
+    (n-1).times do |i|
+      if array[i] > array[i+1]
+        array[i], array[i+1]=array[i+1], array[i]
+        swapped = true
+      end
+    end
+    break if not swapped
+  end
+  array
+end
+
+p bubble_sort(arr)
