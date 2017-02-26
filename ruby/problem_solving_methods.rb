bde0c1747b1143f8594001cf6e4925ff252599c8
Michael Milford <mnmilford@gmail.com>
2017-02-25 21:28:27 -0600

    Change fibonacci method to use .times based on input parameter

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -7,7 +7,7 @@ def search_array(array, number)
   while index < array.length
     array.each do |x|
       if x == number
-        p index
+        index
       end
       index += 1
     end
@@ -16,4 +16,22 @@ end
 
 arr = [42, 89, 23, 1]
 
-search_array(arr, 23)
+# search_array(arr, 23)
+
+# Release 1
+
+def fib(num)
+  array = []
+  num.times do |x|
+    if x == 0
+      array[x] = 0
+    elsif x == 1
+      array[x] = 1
+    else
+      array[x] = array[x-2] + array[x-1]
+    end
+  end
+  array[0..num]
+end
+
+p fib(6)
