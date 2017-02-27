b6576f6aac7a3663936714e34812b3f89e653e80
Michael Milford <mnmilford@gmail.com>
2017-02-26 19:47:20 -0600

    Change parameter name in next_letter method for readability

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -13,24 +13,36 @@ end
 
 def next_letter(name)
   vowels = ["a","e","i","o","u"]
+  upcase_vowels = vowels.map {|i| i.upcase}
   consanants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", 
         "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"]
+  upcase_consanants = consanants.map {|i| i.upcase}
+  
   name = name.downcase
   name = name.chars
-  name.map! do |x|
-    if vowels.include?(x)
-      x = vowels[vowels.index(x).next]
-    elsif consanants.include?(x)
-      x = consanants[consanants.index(x).next]
+
+  name.map! do |letter|
+    if vowels.include?(letter)
+      letter = vowels[vowels.index(letter).next]
+
+    elsif upcase_vowels.include?(letter)
+      letter = upcase_vowels[upcase_vowels.index(letter).next]
+
+    elsif consanants.include?(letter)
+      letter = consanants[consanants.index(letter).next]
+
+    elsif upcase_consanants.include?(letter)
+      letter = upcase_consanants[upcase_consanants.index(letter).next]
+
     else
-      x = " "
+      letter = " "
+
     end
   end
+
   name = name.join('')
   name
 end
-
-
 
 # puts "What is the spy's real name?"
 # spy_name = gets.chomp
