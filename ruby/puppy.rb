d7c6eab237b08c5f822ea5a9b180520fe2bf1129
Michael Milford <mnmilford@gmail.com>
2017-02-27 18:33:22 -0600

    Add puppy class characteristics & behavior

---
+++ ruby/puppy.rb
@@ -0,0 +1,16 @@
+# Puppies
+
+# Characteristics
+# ---------------------------
+# Leg Count: 4
+# Snout Length: varies
+# Energy Level: high
+# Color: varies
+
+# Behavior
+# ---------------------------
+# Run
+# Jump
+# Fetch
+# Bark
+# Roll Over
