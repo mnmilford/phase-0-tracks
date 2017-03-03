0224b25a95ec8c4e86a1447bacae128520d2cbd5
Michael Milford <mnmilford@gmail.com>
2017-03-02 20:20:08 -0600

    Complete Release 0 - Bring Stanta to Life

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,3 +1,19 @@
 class Santa
-  
+
+  def initialize
+    puts "Initializing Santa instance ..."
+  end
+
+  def speak
+    puts "Ho, ho, ho! Haaaappy holidays!"
+  end
+
+  def eat_milk_and_cookies(cookie_type)
+    puts "That was a good #{cookie_type}!"
+  end
+
 end
+
+northpole_king = Santa.new
+northpole_king.speak
+northpole_king.eat_milk_and_cookies("snickerdoodle")
