f27cce5b846f47505260a7d6d6e0a5629611e61e
Michael Milford <mnmilford@gmail.com>
2017-03-03 10:16:21 -0600

    Add driver code to test Shout module/methods

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -4,6 +4,10 @@ module Shout
   end
 
   def self.yelling_happily(words)
-    "OMG! #{words}! That's great to hear! :)"
+    "OMG! " + words.upcase + "! :)"
   end
 end
+
+# Driver code to test Shout module
+puts Shout.yell_angrily("Why do you have to be the way you are?")
+puts Shout.yelling_happily("I love that shirt")
