7c6f22a136b2235953dc46d5a5d8aff0502ea711
Michael Milford <mnmilford@gmail.com>
2017-03-02 21:52:02 -0600

    Add attributes to default Santa in initialize method

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,7 +1,10 @@
 class Santa
-
-  def initialize
+  def initialize(gender, ethnicity)
     puts "Initializing Santa instance ..."
+    @gender = gender
+    @ethnicity = ethnicity
+    @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+    @age = 0
   end
 
   def speak
@@ -11,7 +14,6 @@ class Santa
   def eat_milk_and_cookies(cookie_type)
     puts "That was a good #{cookie_type}!"
   end
-
 end
 
 northpole_king = Santa.new
