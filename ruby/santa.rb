611a49f12e92b574ad60746310fe5ae0698c1df2
Michael Milford <mnmilford@gmail.com>
2017-03-02 23:55:17 -0600

    Set Santa class age attribute to random number 1-140

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,13 +1,13 @@
 class Santa
   attr_reader :ethnicity, :age
-  attr_accessor :gender
+  attr_accessor :genderq
 
   def initialize(gender, ethnicity)
-    puts "Initializing Santa instance ..."
+    # puts "Initializing Santa instance ..."
     @gender = gender
     @ethnicity = ethnicity
     @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
-    @age = 0
+    @age = rand(1..140)
   end
 
   def speak
@@ -32,19 +32,19 @@ class Santa
   end
 end
 
-# northpole_king = Santa.new("male", "white")
-# northpole_king.speak
-# northpole_king.eat_milk_and_cookies("snickerdoodle")
-
+# Program to create 100 Santas
 santas = []
 example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
 example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
-example_genders.length.times do |i|
-  santas << Santa.new(example_genders[i], example_ethnicities[i])
+5.times do
+  santas << Santa.new(example_genders.sample, example_ethnicities.sample)
 end
 
-santas[0].get_mad_at("Bob")
-santas[0].celebrate_birthday
-p santas[0].age
-santas[0].gender = "Apache Helicopter"
-p santas[0]
+# Driver code to test various instance methods
+# santas[0].get_mad_at("Bob")
+# santas[0].celebrate_birthday
+# p santas[0].age
+# santas[0].gender = "Apache Helicopter"
+# p santas[0]
+
+p santas
