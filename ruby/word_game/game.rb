f26f12e8426a5328d686f3b1d240e4c78e95177d
Michael Milford <mnmilford@gmail.com>
2017-03-06 04:13:19 -0600

    Add method to check status of Player 2 guess

--- ruby/word_game/game.rb
+++ ruby/word_game/game.rb
@@ -7,6 +7,19 @@ class WordGame
 
 end
 
+# Method to check the progress of Player 2 guesses
+def status(word, guessed_letters)
+  display_string = ''
+  word.chars.each do |letter|
+    if guessed_letters.include?(letter)
+      display_string << letter
+    else 
+      display_string << '_'
+    end
+  end
+  display_string
+end
+
 puts "Player 1: Enter a word for Player 2 to guess."
 word = gets.chomp
 guesses_left = word.length
