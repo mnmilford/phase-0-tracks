879dc474d6fb8b4ae17a31b8007749789adb710e
Michael Milford <mnmilford@gmail.com>
2017-03-10 13:40:28 -0600

    Pseudocode an algorithm to reverse a string

--- js/explore.js
+++ js/explore.js
@@ -1,2 +1,8 @@
 // Write a JavaScript Program to reverse a string
 
+// Input: The method/function will accept a string (single or multi-word) as the input
+// Convert the string into a list of the characters
+// Calculate the length of the list of characters
+// One-by-one, grab the last character in the list and add it to a NEW list
+// Once grabbing all the characters and putting into a new list,
+// return the new list as a string with the list items combined together
