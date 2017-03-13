512ab78f9c483fc03b251ff34f19bc2ce33df9ab
Michael Milford <mnmilford@gmail.com>
2017-03-13 08:28:55 -0500

    Pseudocode an algorithm to return largest phrase in an array

---
+++ js/algos.js
@@ -0,0 +1,13 @@
+/***********************
+A function to return longest phrase in an array
+Input: 
+  An array of multiple strings. Could be single words or phrases.
+Do: 
+  Set a default value of high_number at 0
+  Calculate the number of characters for each phrase in the array
+  If the number of characters is greater than the current high_number
+  set the phrase as a new variable called longest_phrase
+  Once all phrases in the array have been examined,
+Output:
+  Return the current string stored in the variable longest_phrase
+************************/
