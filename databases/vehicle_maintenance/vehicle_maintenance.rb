f185aef891f6b9698fe90ade3049aaf45173d584
Michael Milford <mnmilford@gmail.com>
2017-03-20 10:02:18 -0500

    Add code to create a database with 2 tables (vehicles & log)

--- databases/vehicle_maintenance/vehicle_maintenance.rb
+++ databases/vehicle_maintenance/vehicle_maintenance.rb
@@ -6,3 +6,36 @@
 # require gems
 require 'sqlite3'
 
+# Create the required SQLite3 database
+db = SQLite3::Database.new("car_maintenance.db")
+db.results_as_hash = true
+
+# Code to generate the required tables
+create_vehicles_table = <<-SQL_CARS
+  CREATE TABLE IF NOT EXISTS vehicles(
+    id INTEGER PRIMARY KEY,
+    year INT,
+    make VARCHAR(255),
+    model VARCHAR(255)
+  )  
+SQL_CARS
+
+create_log_table = <<-SQL_Log
+  CREATE TABLE IF NOT EXISTS log(
+    id INTEGER PRIMARY KEY,
+    service_date VARCHAR(255),
+    car_id INT,
+    mileage INT,
+    service_performed VARCHAR(255),
+    cost FLOAT,
+    FOREIGN KEY (car_id) REFERENCES vehicles(id)
+  )
+SQL_Log
+
+# Create the empty tables
+db.execute(create_vehicles_table)
+db.execute(create_log_table)
+
+# Create some methods to add entries to tables
+def add_vehicle(year, make, model)
+  vehicles
