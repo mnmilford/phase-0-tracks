b218bbd085715b0c1aab4b4f8da5ade329e87bba
Michael Milford <mnmilford@gmail.com>
2017-03-20 09:02:15 -0500

    Initial commit - create vehicle_maintenance.rb file with program goals

---
+++ databases/vehicle_maintenance/vehicle_maintenance.rb
@@ -0,0 +1,8 @@
+# VEHICLE MAINTENANCE LOG
+# Allow user to add different cars to a database
+# Users can keep one log file that shows work done on any cars
+# Log file will keep track of date, mileage, cost of service, and additional notes
+
+# require gems
+require 'sqlite3'
+
